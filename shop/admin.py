2e9c8377408756976abeb0b06f5fd7c13b727dad
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2024-05-25 20:59:53 +0500

    commit

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import Client, ContactApplication, Discount
+
+# admin.site.register(Client)
+# admin.site.register(ContactApplication)
+# admin.site.register(Discount)
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class ContactApplication(models.Model):
+  full_name = models.CharField(max_length=100)
+  phone_number = models.CharField(max_length=20)
+  
+  def __str__(self):
+        return f"{self.id}-{self.full_name}"
+  
+
+class Discount(models.Model):
+  title= models.CharField(max_length=100)
+  percentage = models.PositiveIntegerField()
+  image = models.ImageField(upload_to='discount')
+  link = models.URLField()
+
+  def __str__(self):
+        return f"{self.id}-{self.title}"
+
+class Client(models.Model):
+  class ClientChoices(models.Choices):
+      STARS = 'stars', 'Stars'
+      COMPANY = 'company',"Company"
+  name= models.CharField(max_length=100)
+  position = models.CharField(max_length=50)
+  image = models.ImageField(upload_to='discount')
+  type = models.CharField(max_length=50, choices=ClientChoices.choices, default=ClientChoices.STARS)
+
+  def __str__(self):
+        return f"{self.id}-{self.name}"
+
+
