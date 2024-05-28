e4ec6824eaa8f1db6cd44511238331502a392d5b
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2024-05-28 20:36:05 +0500

    email

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 from . import models
-from .models import Client, ContactApplication, Discount,Faq
+from .models import Client, ContactApplication, Discount, Faq, Email_account
 
 admin.site.register(models.Faq)
+admin.site.register(models.Email_account)
 admin.site.register(models.Repair)
 admin.site.register(models.Product)
 admin.site.register(models.Category)
