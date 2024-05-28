b005f465a6187699bf33720090e8383df095566d
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2024-05-28 13:54:50 +0500

    bakirali

--- shop/admin.py
+++ shop/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import Client, ContactApplication, Discount
 
 admin.site.register(Client)
-# admin.site.register(ContactApplication)
+admin.site.register(ContactApplication)
 # admin.site.register(Discount)
